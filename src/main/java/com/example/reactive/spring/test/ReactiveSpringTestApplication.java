117ad8f94b88a6e4d230a118561040c5681c89df
Chandra Sekhar Battula <99475673+I690290@users.noreply.github.com>
2023-05-16 12:57:06 +0000

    Initial commit

---
+++ src/main/java/com/example/reactive/spring/test/ReactiveSpringTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.reactive.spring.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringTestApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/example/reactive/spring/test/ReactiveSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactive.spring.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
